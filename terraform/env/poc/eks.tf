86a9de1d6d7c057d1db5fa3372270f0a39f7ad46
Varadharajulu, Perumal <perumal98841@gmail.com>
2021-03-15 14:51:18 +0530

    Update ASG

--- terraform/env/poc/eks.tf
+++ terraform/env/poc/eks.tf
@@ -37,8 +37,8 @@ module "eks" {
     {
       name                 = "worker-group-1"
       instance_type        = "t3.small"
-      asg_desired_capacity = 2
-      public_ip            = true
+      asg_desired_capacity = 1
+      public_ip            = false
     },
   ]
 
